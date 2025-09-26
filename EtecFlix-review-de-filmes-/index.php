419549d7a8ad7778b67413de52b30ef266167d9b
JoaoH2512 <161508350+JoaoH2512@users.noreply.github.com>
2025-09-26 16:55:44 -0300

    Add files via upload

---
+++ EtecFlix-review-de-filmes-/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EtecFlix</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+<header>
+  <div class="logo">EtecFlix</div>
+  <nav>
+    <ul>
+      <li><a href="index.php" class="ativo">Início</a></li>
+      <li><a href="sobre.php">Sobre</a></li>
+      <li><a href="login.php">Login</a></li>
+    </ul>
+  </nav>
+</header>
+
+<!-- Conteúdo principal -->
+<main class="container mt-5">
+  <div class="row justify-content-center">
+    
+    <!-- Texto de boas-vindas -->
+    <div class="col-md-7">
+      <h3 class="fw-bold">Bem-vindo a EtecFlix</h3>
+      <p>
+        Este é um site feito na aula de SW I com o objetivo de fazer um site de review de filmes usando html, css e php. Sendo que tem que ter login e uma boa navegação pelo site com um belo css imbutido
+      </p>
+      <a href="login.php" class="btn btn-primary">Ir para Login</a>
+    </div>
+
+
+
+  </div>
+
+  <!-- Rodapé -->
+  <div class="text-center mt-5 text-muted">
+    © Etec MCM 2025 — Feito por João Henrique de Andrade Gimenes
+  </div>
+</main>
+
+</body>
+</html>
